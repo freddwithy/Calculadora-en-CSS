c829f7700472183ee684012520f7cb394e7b3174
freddwithy <134991353+freddwithy@users.noreply.github.com>
2023-05-31 19:28:05 -0400

    Hecho toda la logica de la calculadora

--- app.js
+++ app.js
@@ -10,14 +10,24 @@ botones.forEach(boton => {
             return;
         }
         if (boton.id === "borrar"){
-            if (pantalla.textContent.length === "1"){
+            if (pantalla.textContent.length === 1 || pantalla.textContent === "Error!"){
                 pantalla.textContent = "0";
             } else {
                 pantalla.textContent = pantalla.textContent.slice (0, -1);
             }
             return;
         }
-        if (pantalla.textContent === "0"){
+
+        if (boton.id === "igual"){
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error!"){
             pantalla.textContent = botonApretado;
         } else {
             pantalla.textContent += botonApretado;
